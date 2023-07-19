e64bd2a98cab36f4fcfe14a53a572e631c681c02
Rocio-Peralta <1rocioperalta@gmail.com>
2023-07-19 19:02:52 +1200

    add icons in project

--- README.md
+++ README.md
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
 
 # porfolio-rocio
 
-My porfolio.
-||||||| empty tree
-=======
 
-# React Typescript Tailwind Boilerplate Application
-
-This is a pre-built React project with Tailwind setup.
-
-## Supported Language
-
-- Typescript
-  > > > > > > > master
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,14 +1,59 @@
 function Projects() {
   return (
-    <div className="project-container">
-      <div className="project-card">
-        <img className="project-image" alt="" src="public/image/TODO.png" />
-        <div className="project-background" />
-        <div className="project-content">
-          <h3 className="title">Pinky Promise</h3>
-          <p className="project description"></p>
-          <p className="project skills"></p>
-          <p className="project Links"></p>
+    <div className="project-container pt-12">
+      <div className="project-card relative mb-16 flex flex-row justify-end overflow-hidden rounded-md bg-white shadow-md">
+        <img
+          className="project-image absolute left-0 top-0 w-4/5"
+          alt=""
+          src="/image/TODO.png"
+        />
+        <div
+          className="project-background absolute right-[-1px] top-[-1px] h-[110%] w-[83%] bg-cover"
+          style={{ backgroundImage: "url'/image/project-background-white.svg')" }}
+        />
+      </div>
+      <div className="project-content z-10 w-1/2 p-16">
+        <h3 className="title mb-9 mt-0 text-2xl font-medium leading-[38px] text-gray-500">
+          TODO
+        </h3>
+        <p className="project description">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda quas fugit
+          soluta autem deserunt doloremque quidem nostrum quisquam vero eum, consequatur
+          et in ut. Illum sint at molestiae cumque quaerat!
+        </p>
+        <div className="SKILLS">
+          <span className="skill">HTML</span>
+          <span className="skill">HTML</span>
+          <span className="skill">HTML</span>
+          <span className="skill">HTML</span>
+        </div>
+        <div className="LINKS container flex flex-wrap items-center justify-start">
+          <a
+            href="http://rocio-todo-full-stack.devacademy.nz/"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="hover:fadedPurple mr-5 mt-5 inline-block rounded-md bg-purple px-4 py-0.5 text-lg font-semibold leading-[40px] text-white shadow-lg hover:bg-fadedBlue hover:shadow-md"
+          >
+            {' '}
+            Live Demo
+            <span>
+              {' '}
+              <i className="fas fa-external-link-alt"></i>
+            </span>
+          </a>
+          <a
+            href="http://rocio-todo-full-stack.devacademy.nz/"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="project-link hover:fadedPurple mr-5 mt-5 inline-block rounded-md  px-4 py-0.5 text-lg font-semibold leading-[40px] text-purple shadow-md hover:translate-y-[-1px] hover:bg-slate-200 hover:text-white"
+          >
+            {' '}
+            View Source
+            <span>
+              {' '}
+              <i className="fa-brands fa-github"></i>
+            </span>
+          </a>
         </div>
       </div>
     </div>
